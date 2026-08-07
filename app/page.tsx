0ef5924d0a99ca26aa01eeaee359a87b92f10c67
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-08-07 08:25:16 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container">
+        <h1
+          //test. must be removed
+          style={{
+            color: "black",
+          }}
+          //
+        >
+          Main page
+        </h1>
+      </div>
+    </main>
+  );
+}
