311a0b8f6bd3f64d3d9f3a15687fbae020c0779c
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-08-14 18:21:12 +0300

    save file

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
-import { AboutUs } from "../components/AboutUs/AboutUs";         
-import styles from "./page.module.css";
-import PopularArticles from "@/components/popularArticles/PopularArticles";
-=======
+import styles from "./page.module.css";    
+import PopularArticles from "@/components/popularArticles/PopularArticles";  
 import { AboutUs } from "../components/AboutUs/AboutUs";
 import { TopCreators } from "../components/TopCreators/TopCreators";
->>>>>>> main
-import Hero from "../components/Hero/Hero";
+import Hero from "../components/Hero/Hero"; 
 
 export default function HomePage() {
   return (
     <main>
       <Hero />   
       <AboutUs />
-<<<<<<< HEAD
-      <PopularArticles/>
+      <PopularArticles/>    
+       <TopCreators />
     </main>     
-=======
-      <TopCreators />
-    </main>
->>>>>>> main
-  );
+  ); 
 }
