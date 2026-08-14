cc141c57aef30baf858b62a50a3ae4c05c2b919f
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-08-14 03:15:29 +0200

    fix: resolve main merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,24 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
       },
       {
-        protocol: 'https',
-        hostname: 'ftp.goit.study',
         protocol: "https",
         hostname: "ftp.goit.study",
       },
-       { protocol: "https", 
-         hostname: "res.cloudinary.com" 
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
 };
-
 
 export default nextConfig;
