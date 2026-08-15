08166378b82280ebf2cefb4228d721a7fa424f79
IrynaDiatel <kastyrinaira@gmail.com>
2026-08-15 05:37:30 +0200

    feat: update HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { AboutUs } from "../components/AboutUs/AboutUs";
-import PopularArticles from "@/components/popularArticles/PopularArticles";
-import { TopCreators } from "../components/TopCreators/TopCreators";
 import Hero from "../components/Hero/Hero";
+import { AboutUs } from "../components/AboutUs/AboutUs";
+import PopularArticles from "../components/popularArticles/PopularArticles";
+import { TopCreators } from "../components/TopCreators/TopCreators";
 
 export default function HomePage() {
   return (
