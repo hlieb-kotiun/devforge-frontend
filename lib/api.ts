139f91636a3567f463c55a7da106b04421a838a4
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-08-16 13:08:18 +0200

    Revert "Feature/register form"

--- lib/api.ts
+++ lib/api.ts
@@ -1,27 +1,3 @@
-import axios from "axios";
-
-const nextServer = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true,
-});
-export type RegisterRequest = {
-  email: string;
-  password: string;
-  username: string;
-};
-export type User = {
-  id: string;
-  email: string;
-  userName?: string;
-  photoUrl?: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
-export const register = async (data: RegisterRequest) => {
-  const res = await nextServer.post<User>("/auth/register", data);
-  return res.data;
-};
 import type { Article } from "@/types/article";
 
 export interface ArticlesResponse {
@@ -57,7 +33,6 @@ export async function getArticles(
 
   return response.json();
 }
-
 
 export function getPopularArticles(
   limit: number = 4,
