6cadc2ce2c8ae984ebec3784323e6f847d06ca55
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-16 16:34:29 +0200

    fix: correct form positioning on the page

--- app/(auth-routes)/register/page.tsx
+++ app/(auth-routes)/register/page.tsx
@@ -1,9 +1,10 @@
-import css from "./RegisterPage.module.css"
+import RegisterForm from "@/components/RegisterForm/RegisterForm";
+import css from "./RegisterPage.module.css";
 const RegisterPage = () => {
   return (
     <section className={css.register_section}>
       <div className={`container`}>
-        {/* // <RegisterForm /> */}
+        <RegisterForm />
       </div>
     </section>
   );
