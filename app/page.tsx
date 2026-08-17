5c7f6f8c9b82afbb13853f9831e2afffafae70a5
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-08-17 20:43:37 +0200

    remove loader from home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import { AboutUs } from "../components/AboutUs/AboutUs";
 import PopularArticles from "@/components/popularArticles/PopularArticles";
 import { TopCreators } from "../components/TopCreators/TopCreators";
 import Hero from "../components/Hero/Hero";
-import { Loader } from "@/components/Loader/Loader";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
-      <Loader />
       <AboutUs />
       <PopularArticles />
       <TopCreators />
