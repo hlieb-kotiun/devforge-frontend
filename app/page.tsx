a6f1f6a42b9cc5a5e9fd631105bf1ea491201fd2
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-08-17 20:42:19 +0200

    fix loader position

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { AboutUs } from "../components/AboutUs/AboutUs";
 import PopularArticles from "@/components/popularArticles/PopularArticles";
 import { TopCreators } from "../components/TopCreators/TopCreators";
 import Hero from "../components/Hero/Hero";
+import { Loader } from "@/components/Loader/Loader";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <Loader />
       <AboutUs />
       <PopularArticles />
       <TopCreators />
